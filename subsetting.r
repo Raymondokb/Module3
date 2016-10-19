2368ab71159aa7fdaff5fdcfa261b39d83d84f5c
raymondokb <raymondokbmooc@gmail.com>
2016-10-19 15:55:53 +0800

    Quiz 3 done

--- subsetting.r
+++ subsetting.r
@@ -60,8 +60,47 @@ quantile(readJPEG(rawjpeg, TRUE), probs = c(0, 0.3, 0.8, 1))
 
 
 ##
-gdp = read.csv("D:\\Users\\user\\Downloads\\getdata%2Fdata%2FGDP.csv")
-country = read.csv( "D:\\Users\\user\\Downloads\\getdata%2Fdata%2FEDSTATS_Country.csv")
+gdp = as.data.frame(read.csv("D:\\Users\\user\\Downloads\\getdata%2Fdata%2FGDP.csv", stringsAsFactors  =FALSE))
+gdp=gdp[5:194,1:5]; gdp = gdp[, c(1,2,4,5)]
+country = as.data.frame(read.csv( "D:\\Users\\user\\Downloads\\getdata%2Fdata%2FEDSTATS_Country.csv", stringsAsFactors =FALSE))
 country$gdp = 1
 
 dim(country)
+
+which(country$CountryCode=="USA")
+which(gdp$X=="USA")
+#Hmm
+number=""
+for(i in 1:dim(country)[1]){
+  indexes = which(gdp$X==country$CountryCode[i])
+  if(length(gdp[indexes,2])==0 ){
+    country$gdp[i] = NA
+  }
+  else{
+    country$gdp[i] = gdp[indexes,2]
+  }
+}
+sum(is.na(country$gdp))+1
+
+sorted <- as.numeric(sort(country$gdp, decreasing=TRUE, na.last=TRUE))
+which(country$gdp==sort(country$gdp, decreasing=TRUE, na.last=TRUE)[13])
+country$Long.Name[144]
+
+number=""
+numlist= as.numeric(unique(na.omit(as.numeric(unlist(strsplit(ww, "[^0-9]+"))))))
+for(i in 1:length(numlist)){
+  number = paste(c(number,numlist[i]), collapse="")
+}
+
+
+#
+
+index1 = which(country$Income.Group=="High income: OECD")
+index2 = which(country$Income.Group=="High income: nonOECD")
+
+mean(as.numeric(country[index1,]$gdp))
+mean(as.numeric(country[index2,]$gdp),na.rm=TRUE)
+
+index3 = which(country$Income.Group=="Lower middle income")
+sorted2 = sort(as.numeric(country[index3,]$gdp))
+sum(sorted2<=38)
