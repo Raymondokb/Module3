6a701c97e4065891e8611a227478e841eb50b7b8
raymondokb <raymondokbmooc@gmail.com>
2016-10-19 10:26:32 +0800

    ll

--- tester.r
+++ tester.r
@@ -3,3 +3,14 @@ url <- "http://scholar.google.com/citations?user=HI-I6C0AAAAJ"
 html <- htmlTreeParse(url, useInternalNode=T)
 
 xpathSApply(html, "//title", xmlValue)
+
+xpathSApply(html, "//td[@class='gsc_a_c']", xmlValue) #using web scraping learned in PIC 97 :D
+
+#Alternative way using HTTR package library
+library(httr); html2 = GET(url)
+content2 = content(html2, as='text')
+parsedHtml = htmlParse(content2, asText=TRUE)
+xpathApply(parsedHtml, "//title", xmlValue)
+
+
+## Accessing websites with passwords
