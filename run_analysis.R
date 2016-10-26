0f5dcc8572892d24ee68566ce799466cea0c5869
raymondokb <raymondokbmooc@gmail.com>
2016-10-26 16:51:33 +0800

    halfway through m3 project

--- run_analysis.R
+++ run_analysis.R
@@ -1,18 +1,53 @@
-activity_label <- read.table("D:\\Users\\user\\Downloads\\getdata_2Fprojectfiles%2FUCI HAR Dataset\\UCI HAR Dataset\\activity_labels.txt")
-features <- read.table("D:\\Users\\user\\Downloads\\getdata_2Fprojectfiles%2FUCI HAR Dataset\\UCI HAR Dataset\\features.txt")
+activity_label <- read.table("D:\\Users\\user\\Downloads\\getdata_2Fprojectfiles%2FUCI HAR Dataset\\UCI HAR Dataset\\activity_labels.txt",stringsAsFactors = FALSE)
+features <- read.table("D:\\Users\\user\\Downloads\\getdata_2Fprojectfiles%2FUCI HAR Dataset\\UCI HAR Dataset\\features.txt", stringsAsFactors = FALSE)
 
-test_subject_test <- read.table("D:\\Users\\user\\Downloads\\getdata_2Fprojectfiles%2FUCI HAR Dataset\\UCI HAR Dataset\\test\\subject_test.txt")
-test_x_test <- read.table( "D:\\Users\\user\\Downloads\\getdata_2Fprojectfiles%2FUCI HAR Dataset\\UCI HAR Dataset\\test\\X_test.txt")
-test_y_test <- read.table("D:\\Users\\user\\Downloads\\getdata_2Fprojectfiles%2FUCI HAR Dataset\\UCI HAR Dataset\\test\\y_test.txt")
+test_subject_test <- read.table("D:\\Users\\user\\Downloads\\getdata_2Fprojectfiles%2FUCI HAR Dataset\\UCI HAR Dataset\\test\\subject_test.txt", stringsAsFactors = FALSE)
+test_x_test <- read.table( "D:\\Users\\user\\Downloads\\getdata_2Fprojectfiles%2FUCI HAR Dataset\\UCI HAR Dataset\\test\\X_test.txt", stringsAsFactors = FALSE)
+test_y_test <- read.table("D:\\Users\\user\\Downloads\\getdata_2Fprojectfiles%2FUCI HAR Dataset\\UCI HAR Dataset\\test\\y_test.txt", stringsAsFactors = FALSE)
 
 
-train_subject_train <- read.table("D:\\Users\\user\\Downloads\\getdata_2Fprojectfiles%2FUCI HAR Dataset\\UCI HAR Dataset\\train\\subject_train.txt")
-train_x_train <- read.table("D:\\Users\\user\\Downloads\\getdata_2Fprojectfiles%2FUCI HAR Dataset\\UCI HAR Dataset\\train\\X_train.txt")
-train_y_train <- read.table("D:\\Users\\user\\Downloads\\getdata_2Fprojectfiles%2FUCI HAR Dataset\\UCI HAR Dataset\\train\\y_train.txt")
+train_subject_train <- read.table("D:\\Users\\user\\Downloads\\getdata_2Fprojectfiles%2FUCI HAR Dataset\\UCI HAR Dataset\\train\\subject_train.txt", stringsAsFactors = FALSE)
+train_x_train <- read.table("D:\\Users\\user\\Downloads\\getdata_2Fprojectfiles%2FUCI HAR Dataset\\UCI HAR Dataset\\train\\X_train.txt", stringsAsFactors = FALSE)
+train_y_train <- read.table("D:\\Users\\user\\Downloads\\getdata_2Fprojectfiles%2FUCI HAR Dataset\\UCI HAR Dataset\\train\\y_train.txt", stringsAsFactors =FALSE)
 
 grep("mean",features$V2)
 grep("std", features$V2)
 
 
 #union(grep("mean",features$V2),grep("std", features$V2) )
-table(test_y_test)
+#library(dplyr)
+#test_x_test <- tbl_df(test_x_test)
+#test_y_test <- tbl_df(test_y_test)
+#train_x_train <- tbl_df(train_x_train)
+#train_y_train <- tbl_df(train_y_train)
+
+#activity_label <- tbl_df(activity_label)
+#features <- tbl_df(features)
+#test_subject_test <- tbl_df(test_subject_test)
+#train_subject_train <- tbl_df(train_subject_train)
+
+for(i in 1:dim(test_y_test)[1]){
+  test_y_test$descript[i] = activity_label[test_y_test[[1]][i],2]
+}
+for(i in 1:dim(train_y_train)[1]){
+  train_y_train$descript[i] = activity_label[train_y_train[[1]][i],2]
+}
+
+#Now, time to play
+
+all.data <- rbind(train_x_train, test_x_test)
+grep("mean",features$V2)
+grep("std", features$V2)
+union_index <- union(grep("mean",features$V2),grep("std", features$V2) )
+
+t_all.data <- t(all.data)
+subsetted <- t_all.data[union_index,]
+all.data2 <- as.data.frame(t(subsetted))
+
+indicators.full <- rbind(train_y_train, test_y_test)
+all.data2 <- cbind(all.data2, indicators.full$descript)
+
+colnames(all.data2) <- c(features[union_index,2],"Description")
+
+
+
